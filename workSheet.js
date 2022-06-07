0acfc25a29704473bf3a18b089492903515f9b62
Seabro09 <43609896+Seabro09@users.noreply.github.com>
2022-06-07 09:16:20 -0400

    Update workSheet.js

--- workSheet.js
+++ workSheet.js
@@ -1,26 +1,5 @@
-// ADD CODE HERE
 
-function reduce(array, instructions, initialValue) {
-    if (Array.isArray(array)) {
-  		let accumulator = initialValue;
-  		let totals = 0;
-    	for (let i = 0 ; i < array.length; i++) {
-      	accumulator = instructions(accumulator, array[i])
-    		totals = accumulator
-    	}
-  	return totals;
-	}
-  return "The first argument should be an array";
-}
-
-
-// Uncomment these to check your work!
-// const arr1 = [5, 10, 15, 20];
-// const arr2 = [15, 88, 1, 5, 7];
-// const arr3 = [1, 10, 15, 5, 20];
-// console.log(intersection([arr1, arr2, arr3])); // should log: [5, 15]
-
-// ADD CODE HERE
+//////////////////////////////////////////////////////////////////
 
 function intersection(arr1, arr2, arr3) {
     let finalArray = [];
