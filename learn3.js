3f974c0ef3b6cce3e0ec4b0951dfd39fd9f215cb
Seabro09 <43609896+Seabro09@users.noreply.github.com>
2022-06-24 10:04:00 -0400

    Continuing with Closure Training

--- learn3.js
+++ learn3.js
@@ -256,3 +256,27 @@ console.log(getDay1()); // should log: 'Fri'
 console.log(getDay1()); // should log: 'Sat'
 
 ////////////////////////////////////////////////////////////////////////////////////////////
+
+// Challenge: defineFirstArg
+
+//Create a function defineFirstArg that accepts a function and an argument. Also, the function being passed in will accept at least one argument. defineFirstArg will return a new function that invokes the passed-in function with the passed-in argument as the passed-in function's first argument. Additional arguments needed by the passed-in function will need to be passed into the returned function.
+
+// My Code
+
+function defineFirstArg(callback, argu) {
+  let num = 0;
+    function returnSubtract(...args) { // spread operator so that the returned function can accept multiple arguments
+      num = callback(argu, ...args);
+      return num;
+    }
+  return returnSubtract;
+}
+// Uncomment these to check your work!
+const subtract = function(big, small) { 
+return big - small; 
+};
+
+const subFrom20 = defineFirstArg(subtract, 20);
+console.log(subFrom20(5)); // should log: 15
+
+///////////////////////////////////////////////////////////////////////////////////
