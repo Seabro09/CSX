01c3640ef52e933025ccc93eb73d4f7e694417f5
Seabro09 <43609896+Seabro09@users.noreply.github.com>
2022-07-06 07:32:13 -0400

    Update learn4.js

--- learn4.js
+++ learn4.js
@@ -123,3 +123,69 @@ function flow(input, funcArray) {
 	const output = funcArray[0](input);
  return flow(output, funcArray.slice(1)); // on the first recursive call, pass in funcArray but slice off the first function
 }
+
+
+
+
+//////////////////////////////////////////
+
+
+//Write a function that takes two arrays as inputs, representing the top and bottom halves of a deck of cards, and shuffles them together. The function will return a single array containing the elements from both input arrays interleaved, like so:
+//
+//the first element should be the first element of the first input array,
+//
+//the second element should be the first element of the second input array,
+//
+//the third element should be the second element of the first input array,
+//
+//the fourth element should be the second element of the second array,
+//
+//and so on.
+//
+//The arrays may be of different lengths. After interleaving the elements of the input arrays, any remaining elements should be appended to the end of the array.
+
+// // Two Ways:
+
+// 1.
+ function shuffleCards(topHalf, bottomHalf, newArr = []) { // add a new array as a param
+ 	//base case - if no more elements in first array AND no more elements in second array, return the new array
+ 	if ((topHalf.length === 0) && (bottomHalf.length === 0)) return newArr;
+  
+   // if there are elements in the first array, add the first element to the new array.  Then move on to the bottom half
+   // console.log(topHalf);
+   // console.log(bottomHalf);
+   	if (topHalf.length !== 0) newArr.push(topHalf.shift());
+    
+   // if there are elements in the second array, add that first element to the new array, Then move on to the recursive call
+   	if (bottomHalf.length !== 0) newArr.push(bottomHalf.shift()); 
+  
+   return shuffleCards(topHalf, bottomHalf, newArr)
+ //return the function and pass in the two halves plus the new array
+ }
+
+/////////
+
+// 2.
+function shuffleCards(topHalf, bottomHalf,results = []) {
+    if(topHalf.length === 0 && bottomHalf.length === 0) return results;
+    if (topHalf.length !== 0) results.push(topHalf[0])
+    if (bottomHalf.length!==0) results.push(bottomHalf[0])
+    return shuffleCards(topHalf.slice(1), bottomHalf.slice(1),results); // we can slice off the first element as a parameter in the recursive call 
+}
+
+
+// UNCOMMENT TO TEST YOUR WORK
+const topHalf = ['Queen of Diamonds', 'Five of Hearts', 'Ace of Spades', 'Eight of Clubs'];
+const bottomHalf = ['Jack of Hearts', 'Ten of Spades'];
+console.log(shuffleCards(topHalf, bottomHalf));
+  /*-> ['Queen of Diamonds',
+        'Jack of Hearts',
+        'Five of Hearts',
+        'Ten of Spades',
+        'Ace of Spades',
+        'Eight of Clubs',
+      ]
+  */
+
+
+////////////////////////////////////////////////////////////////
