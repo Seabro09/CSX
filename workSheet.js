5094741b113e01386d5706de400dc10ec30d1661
Seabro09 <43609896+Seabro09@users.noreply.github.com>
2022-07-07 16:22:32 -0400

    Update workSheet.js

--- workSheet.js
+++ workSheet.js
@@ -1,18 +1,7 @@
-function flow(input, funcArray, i = 0, output = 0) {
-	if (i === funcArray.length) {
-    return input;
-  }
-  output = funcArray[i](input);
-  input = output;
-  console.log(output);
-  i++;
-  return flow(input, funcArray, i)
+let factorial = function fac(num) {
+    if (num ===1) return 1;
+    return num * fac(num-1);
+    
+let final = factorial(5);
+console.log(final);
 }
-
-// To check if you've completed the challenge, uncomment this code!
-function multiplyBy2(num) { return num * 2; }
-function add7(num) { return num + 7; }
-function modulo4(num) { return num % 4; }
-function subtract10(num) { return num - 10; }
-const arrayOfFunctions = [multiplyBy2, add7, modulo4, subtract10];
-console.log(flow(2, arrayOfFunctions)); // -> -7
