76049df18809f9bd6b2ae7a7108cf17604089772
Seabro09 <43609896+Seabro09@users.noreply.github.com>
2022-07-08 14:34:54 -0400

    Update workSheet.js

--- workSheet.js
+++ workSheet.js
@@ -7,3 +7,27 @@ function multiplyBy2 (inputnumber) {
 const output = multiplyBy2(4);
 const newOutput = multiplyBy2(10);
 
+/////////
+
+
+function display(data) {
+    console.log(data)
+}
+
+const dataFromAPI = fetchAndWait('https://twitter.com/will/tweets/1')
+
+display(dataFromAPI)
+
+console.log("Me later!");
+
+///////
+
+
+function printHello() {
+    console.log("Hello");
+}
+
+setTimeout(printHello, 1000);
+
+console.log("Me first!");
+
