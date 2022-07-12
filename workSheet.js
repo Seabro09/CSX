c66deb92a309d1e69d7bec744ba7311502582975
Seabro09 <43609896+Seabro09@users.noreply.github.com>
2022-07-12 15:27:10 -0400

    Update workSheet.js

--- workSheet.js
+++ workSheet.js
@@ -27,7 +27,35 @@ function printHello() {
     console.log("Hello");
 }
 
-setTimeout(printHello, 1000);
+setTimeout(printHello, 2000);
 
 console.log("Me first!");
 
+/////
+
+function display(data) {
+    console.log(data);
+}
+
+const futureData = fetch('https://twitter.com/will/tweets/1')
+
+futureData.then(display); // attaches display functionality
+
+console.log("Me First!");
+
+
+////////////
+
+function display(data){console.log(data)}
+function printHello(){console.log("Hello");}
+function blockFor300ms(){/*blocks js thread for 300 ms with long for loop */}
+
+setTimeout(printHello, 0);
+
+const futureData = fetch('https://twitter.com/will.tweets/1')
+
+blockFor300ms();
+
+// Which will run first?
+
+console.log("Me first!");
