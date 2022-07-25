2db98dad27f74b048c2058a9325d2b0a8e56d9e3
Seabro09 <43609896+Seabro09@users.noreply.github.com>
2022-07-25 14:48:57 -0400

    Chrome Extension Project

--- chrome/main.js
+++ chrome/main.js
@@ -1,11 +1,14 @@
-// JavaScipt
+const img1 = document.createElement("img");
+img1.setAttribute("src", "https://images.unsplash.com/photo-1508558936510-0af1e3cccbab?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80");
+const newPara = document.createElement("p");
+const text = document.createTextNode("\"When an action grows unprofitable, gather information; when information grows unprofitable, sleep.\" - Ursula K. Le Guin");
 
-//const contents = document.getElementById("content");
-//content.parentNode.removeChild(content);
-
-
-
-//jquery
-
+newPara.appendChild(text);
+const ytd = $("ytd-app")
 const contents = $("#content");
+const ytdParent = ytd.parent();
+ytd.remove();
 contents.remove();
+ytdParent.prepend(newPara);
+ytdParent.prepend(img1);
+
