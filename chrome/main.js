fe61ac6ba7be05587b9143e3a9e3bd2b7d0d779c
Seabro09 <43609896+Seabro09@users.noreply.github.com>
2022-08-01 09:05:55 -0400

    Chrome Extension Changes

--- chrome/main.js
+++ chrome/main.js
@@ -1,13 +1,13 @@
-const img1 = document.createElement("img");
-img1.setAttribute("src", "https://images.unsplash.com/photo-1508558936510-0af1e3cccbab?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80");
-const div = document.createElement("div");
-const text = document.createTextNode("\"When an action grows unprofitable, gather information; when information grows unprofitable, sleep.\" - Ursula K. Le Guin");
-div.appendChild(text);
-$("div").last().addClass("beautText");
-const ytd = $("ytd-app")
-const contents = $("#content");
+const img1 = document.createElement("img"); // create new constant img1 of type image using the .createElement method
+img1.setAttribute("src", "https://images.unsplash.com/photo-1508558936510-0af1e3cccbab?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"); // add the link to what the image will be using .setAttribute method
+const div = document.createElement("div"); // create constant div of type div
+const text = document.createTextNode("\"When an action grows unprofitable, gather information; when information grows unprofitable, sleep.\" - Ursula K. Le Guin"); // create constant text with our added quote as the value using the .createTextNode method
+div.appendChild(text); // append text as a child to div using the .appendChild method
+div.classList.add("beautText"); // add the class that we have stored in our index.css file using the .classList method
+const ytd = $("ytd-app") // create a constant called ytd and set it equal to the tag name ytd-app.  Here we are using jquery
+const contents = $("#content"); 
 const ytdParent = ytd.parent();
 ytd.remove();
 contents.remove();
 ytdParent.prepend(img1);
-ytdParent.prepend(div);
+ytdParent.prepend(div)
