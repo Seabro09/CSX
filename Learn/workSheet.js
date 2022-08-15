08f5ddbdad3a35a55c339fe33ba29d12b48ac0dd
Seabro09 <43609896+Seabro09@users.noreply.github.com>
2022-08-15 10:57:12 -0400

    Material to review Later
    Nested Objects and deeply comparing objects using recursion.

--- Learn/workSheet.js
+++ Learn/workSheet.js
@@ -1,97 +1,21 @@
-const user1= { 
-    name: "Will",
-    score: 3,
-    increment: function() {
-        user1.score++;
-    }
-};
-
-const user2 = {}; // create an empty object
-
-
-
-//assign properties to that object
-user2.name = "Tim";
-user2.score = 6;
-user2.increment = function() {
-    user2.score++;
-};
-
-
-const user3 = Object.create(null);
-
-user3.name = "Eva";
-user3.score = 9;
-user3.incremenet = function() {
-    user3.score++; 
-};
-
-function userCreator(name, score) {
-    const newUser = {};
-    newUser.name = name;
-    newUser.score = score;
-    newUser.increment = function() {
-        newUser.score++;
-    }
-    return newUser;
-}
-
-userCreator("Will", 3);
-userCreator("Mike", 5);
-
-
-/////////////////////////////////////////
-function userCreator (name, score) {
-    const newUser = Object.create(userFunctionStore);
-    newUser.name = name;
-    newUser.score = score;
-    return newUser;
-};
-
-const userFunctionStore = {
-    increment: function(){this.score++},
-    login: function(){console.log("Logged in");}
-};
-
-const user1 = userCreator("Will", 3);
-const user2 = userCreator("Tim",  5);
-user1.increment();
-
-
-/////////////////////////////////////
-
-function UserCreator(name, score) { // notice the uppercase
-    
-}
-
-UserCreator.prototype.increment = function(){
-    this.score++;
-};
-
-UserCreator.prototype.login = function() {
-    console.log("login");
-}
-
-const user1 = new UserCreator("Eva", 9)
-
-user1.increment() 
-
-
-/////////////////////////////////
-
-class UserCreator {
-    constructor (name,score) {
-        this.name = name;
-        this.score = score;
-    }
-    increment () {
-        this.score++;
-    }
-    login () {
-        console.log("login");
+function deepEqual(obj, obj) {
+    if (typeof (obj) === typeof (obj)) {
+        return true;
+    } else {
+        return false;
     }
 }
 
-const user1 = new UserCreator("Eva", 9);
-
-user1.increment();
+let obj = {
+    here: {
+        is: "an"
+    },
+    object: 2
+};
+console.log(deepEqual(obj, obj));
+// → true
+console.log(deepEqual(obj, {
+    here: 1,
+    object: 2
+}));
+// → false
