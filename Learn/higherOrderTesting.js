c6bbb11c8998b8ac3e07c6697e54ec74d3f30359
Seabro09 <43609896+Seabro09@users.noreply.github.com>
2022-08-17 15:47:56 -0400

    ELO JS exercises, Haverbeke

--- Learn/higherOrderTesting.js
+++ Learn/higherOrderTesting.js
@@ -1,23 +1,4 @@
-let arrays = [[1, 2, 3], [4, 5], [6]];
-arrays.reduce((previousValue, currentValue) => previousValue.concat(currentValue));
-// → [1, 2, 3, 4, 5, 6]
-
-///////////////////////////////////////////////////
-
-function loop(value, testFunc, upFunc) {
-    if (!testFunc(value)) {
-        return;
-    }
-    console.log(value);
-    loop(upFunc(value), testFunc, upFunc);
-}
-
-loop(3, n => n > 0, n => n - 1);
-// → 3
-// → 2
-// → 1
-
-////////////////////////////////////////////////////////////////
+// Thinking about higher order testing with booleans
 
 function every(array, test) {
     let tFalse;
@@ -44,5 +25,3 @@ console.log(every([4, 5, 67, 3, 78], n => n < 10));
 console.log(every([3, 4, 5], n => n > 10));
 
 console.log(every([74, 85, 93], n => n > 10));
-
-//////////////////////////////////////////////////////////
--- chrome/main1.js
+++ chrome/main1.js
@@ -3,30 +3,34 @@
 let clientID = "SWLEVwRUpyzEsprgJwJVPKQ2WBb4WqY-2zklS6gsHcU";
 let endpoint = `https://api.unsplash.com/photos/random/?client_id=${clientID}`;
 const img = document.createElement("img");
-$.ajax({ // initial ajax call to get the random image from unsplash
-    method: `Get`,
-    url: endpoint,
-    success: function (result) {
-        img.src = result.urls.regular;
-    },
-    error: function (err) {
-        alert("Error retrieving data");
-    }
+$.ajax({
+  // initial ajax call to get the random image from unsplash
+  method: `Get`,
+  url: endpoint,
+  success: function (result) {
+    img.src = result.urls.regular;
+  },
+  error: function (err) {
+    alert("Error retrieving data");
+  },
 });
 
-const setBg = () => { //declaration of random color function for the text) 
-    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
-    textDiv.style.color = "#" + randomColor;
-}
+const setBg = () => {
+  //declaration of random color function for the text)
+  const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+  textDiv.style.color = "#" + randomColor;
+};
 
 const textDiv = document.createElement("div"); // create constant div of type div
-const text = document.createTextNode("\"When an action grows unprofitable, gather information; when information grows unprofitable, sleep.\" - Ursula K. Le Guin"); // create constant text with our added quote as the value using the .createTextNode method
+const text = document.createTextNode(
+  '"When an action grows unprofitable, gather information; when information grows unprofitable, sleep." - Ursula K. Le Guin'
+); // create constant text with our added quote as the value using the .createTextNode method
 textDiv.appendChild(text); // append text as a child to div using the .appendChild method
 textDiv.classList.add("beautText"); // add the class that we have stored in our index.css file using the .classList method
 textDiv.classList.add("center");
 
 const skeleton = $("#home-page-skeleton"); // place the skeleton in a constant called skeleton
-const ytd = $("ytd-app") // create a constant called ytd and set it equal to the tag name ytd-app.  Here we are using jquery
+const ytd = $("ytd-app"); // create a constant called ytd and set it equal to the tag name ytd-app.  Here we are using jquery
 const contents = $("#content");
 const ytdParent = ytd.parent();
 
@@ -34,39 +38,45 @@ ytd.remove();
 contents.remove();
 skeleton.remove(); // remove the unnecessary skeleton
 
-
 ytdParent.prepend(img);
 
 ytdParent.prepend(textDiv);
 
-$("div").animate({
+$("div").animate(
+  {
     paddingRight: "100px",
-}, 3000, "linear");
+  },
+  3000,
+  "linear"
+);
 
-$('div').click(function () { // when an element of type div is clicked, hide the text
-    if (textDiv.style.display === "none") {
-        textDiv.style.display = "block";
-    } else {
-        textDiv.style.display = "none";
-    }
+$("div").click(function () {
+  // when an element of type div is clicked, hide the text
+  if (textDiv.style.display === "none") {
+    textDiv.style.display = "block";
+  } else {
+    textDiv.style.display = "none";
+  }
 });
 
-setInterval(function () { // every 5 seconds, bring the text back if style is none.
-    if (textDiv.style.display === "none") {
-        textDiv.style.display = "block";
-    }
-    setBg(); // call the random color change function
-}, 4000)
+setInterval(function () {
+  // every 5 seconds, bring the text back if style is none.
+  if (textDiv.style.display === "none") {
+    textDiv.style.display = "block";
+  }
+  setBg(); // call the random color change function
+}, 4000);
 
-img.onclick = function () { //on click, image will change. 
-    $.ajax({
-        method: `Get`,
-        url: endpoint,
-        success: function (result) {
-            img.src = result.urls.regular;
-        },
-        error: function (err) {
-            alert("Error retrieving data");
-        }
-    });
-}
+img.onclick = function () {
+  //on click, image will change.
+  $.ajax({
+    method: `Get`,
+    url: endpoint,
+    success: function (result) {
+      img.src = result.urls.regular;
+    },
+    error: function (err) {
+      alert("Error retrieving data");
+    },
+  });
+};
