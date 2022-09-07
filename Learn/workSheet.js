03a5c27daf1d47e449f1499179d911b82800b35b
Seabro09 <43609896+Seabro09@users.noreply.github.com>
2022-09-07 12:47:21 -0400

    Working with filter Method

--- Learn/workSheet.js
+++ Learn/workSheet.js
@@ -1,59 +1,31 @@
-class Group {
-    constructor() {
-        this.members = [];
-    }
-    add(value) {
-        if (!this.members.includes(value)) {
-          return this.members.push(value);   
-        } else {
-            console.log("This object already contains this value")
-        }
-    }
-    delete(value) {
-        if (this.members.includes(value)) {
-            const index = this.members.indexOf(value);
-            if (index > -1) {
-                this.members.splice(index, 1);
-            }
-        } else {
-            console.log(`That value is not present`)
-        }
-    }
-    has(value) {
-        if (this.members.includes(value)) {
-            return true;
-        }
-    }
-    static from(values) {
-        let group = new Group;
-        for (let value of values){
-            group.add(value);
-        }
-        return group;
-    }
+// filter
 
-}
-
-const group1 = new Group(); 
-
-group1.add(1);
-group1.add(1);
-group1.add(2);
-group1.add(3);
-group1.add(4);
-group1.add(4);
-group1.delete(1);
-group1.delete(3);
-console.log(group1.has(2));
-
-const group2 =Group.from([10,20])
-group2;
-
-const groupIterator = {
-    next() {
-
+const boats = [
+    {
+        name: "DownTheBayou",
+        weight: 3,
+        length: 5,
     },
-    [symbol.iterator] () {
-        return this;
+    {
+        name: "OntheHighway",
+        weight: 56,
+        length: 27,
+        
     },
-},
+    {
+        name: "HeavyWinds",
+        weight: 3,
+        length: 5,
+    },
+    {
+        name: "CalmSeas",
+        weight: 39,
+        length: 30,
+        
+    },
+];
+
+const filteredByWeight = boats.filter(weight => boats.weight > 3); 
+
+console.log(filteredByWeight);
+console.log(boats.filter(weight => boats.weight > 39));
