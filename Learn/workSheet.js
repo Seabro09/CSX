c769f66e3f864c9ce362833f9bd49ed097c9666d
Seabro09 <43609896+Seabro09@users.noreply.github.com>
2022-09-08 07:05:18 -0400

    Robot Project

--- Learn/workSheet.js
+++ Learn/workSheet.js
@@ -1,31 +1,10 @@
-// filter
+const person = {
+    speak: function() {
+        console.log(this.phrase);
+    }
+}
 
-const boats = [
-    {
-        name: "DownTheBayou",
-        weight: 3,
-        length: 5,
-    },
-    {
-        name: "OntheHighway",
-        weight: 56,
-        length: 27,
-        
-    },
-    {
-        name: "HeavyWinds",
-        weight: 3,
-        length: 5,
-    },
-    {
-        name: "CalmSeas",
-        weight: 39,
-        length: 30,
-        
-    },
-];
-
-const filteredByWeight = boats.filter(weight => boats.weight > 3); 
-
-console.log(filteredByWeight);
-console.log(boats.filter(weight => boats.weight > 39));
+const graph = Object.create(person);
+graph.phrase = "Hello, Paul";
+graph;
+//graph.speak();
