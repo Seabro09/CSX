dd6cfcbd280c77bc4e231f578a19066a18c8ccaf
Seabro09 <43609896+Seabro09@users.noreply.github.com>
2022-09-14 15:07:05 -0400

    Update workSheet.js

--- Learn/workSheet.js
+++ Learn/workSheet.js
@@ -1,14 +1,28 @@
 let array1 = [1,2,3,4];
 let array2 = [5,6,7,8];
 
-//let sum = array1.map(function  (num, idx) {
-//                    return num + array2[idx];
-//});
-//
-//console.log(sum);
-
-function union(arrays) {
-   console.log(arrays[0[0]]);
+function union2(array1, array2) {
+    let finArr = [];
+    for (let i = 0; i < array1.length; i++) {
+    finArr.push(array1[i] + array2[i])
+    }
+return finArr;
 }
 
-union([array1, array2]);
+console.log(union2(array1, array2));
+
+
+function union(arrays) {
+    return arrays.reduce((sumArray, currentArray) => {
+        currentArray.forEach(el => {
+            sumArray.push(el);
+        })
+        return sumArray;
+    }, [])
+}
+
+const finArr = union([array1, array2])
+
+console.log(finArr.reduce((previousValue, currentValue) => previousValue + currentValue
+));
+
